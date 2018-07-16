d9b94d1cc2efb71b780de32517cfd365fd015545
Steban Machado Domingues <steban.md@gmail.com>
2018-07-16 17:34:26 -0300

    Add template

---
+++ AdminTemplate/AdminTemplate/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminTemplate.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+                return RedirectToAction("Index", "Home");
+            return View();
+        }
+
+        public IActionResult ForgottenPassword() => View();
+
+        [Authorize]
+        public IActionResult ManageAccount() => View();
+
+        [HttpPost]
+        public IActionResult ForgottenPassword(string email)
+        {
+            if (string.IsNullOrEmpty(email))
+                ViewData["error"] = true;
+            else
+                ViewData["send"] = true;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string pass)
+        {
+            if (string.IsNullOrEmpty(email))
+                return RedirectToAction(nameof(Login));
+
+            var identity = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name, email),
+            }, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var principal = new ClaimsPrincipal(identity);
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                principal);            
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction(nameof(Login));
+        }
+    }
+}
